fe5a1ef108f3c2da5a98c69cf327242681d2ecf6
Jean Flower <jeanflower@rocketmail.com>
2020-01-01 15:19:21 +0000

    Delete reference to 'product' tutorial example

--- src/routes/finkitty.route.js
+++ src/routes/finkitty.route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const finkitty_controller = require('../controllers/finkitty.controller');
 
 router.get('/test', finkitty_controller.test);
-router.post('/create', finkitty_controller.model_create);
-router.get('/find', finkitty_controller.model_details);
 router.get('/models', finkitty_controller.model_list);
+router.get('/find', finkitty_controller.model_details);
 router.put('/update', finkitty_controller.model_update);
+router.post('/create', finkitty_controller.model_create);
 router.delete('/delete', finkitty_controller.model_delete);
 module.exports = router;
